815deca4599c642d397422774b0d5437c474c246
Debora Jaimes <deboranjaimes@gmail.com>
2021-09-18 17:59:23 -0400

    Primeiro Commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function hello(){
+    alert('Hey bro!');
+    document.getElementById("id-face").src = "https://image.flaticon.com/icons/png/512/132/132255.png";
+}
+
+
+ function iDontLikeU(){
+    alert('ok! :(');
+    document.getElementById("id-face").src = "https://image.flaticon.com/icons/png/512/132/132244.png";
+ }
+
+ function wonderful(){
+     alert('Tu tambem');
+     document.getElementById("id-face").src = "https://image.flaticon.com/icons/png/512/132/132255.png";
+ }
+
+ function trash(){
+    alert('apagado');
+    document.getElementById("id-face").src = "https://image.flaticon.com/icons/png/512/132/132233.png";
+}
+
+
